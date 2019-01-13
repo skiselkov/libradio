skiselkov/libradio

commit 7324e48148e1161c51a5af41cfa08a66ed607f90

Reimplemented LOC alignment code to work around differences in headings between WGS84 and spherical coordinates.

src/navaiddb.c
@@ -733,7 +733,19 @@ loc_align_with_rwy(navaiddb_t *db, navaid_t *nav)
 	if (arpt != NULL &&
 	    airport_find_runway(arpt, nav->loc.rwy_id, &rwy, &end) &&
 	    fabs(rel_hdg(nav->loc.brg, rwy->ends[end].hdg)) <= 1) {
-		nav->loc.brg = rwy->ends[end].hdg;
+		fpp_t fpp = gnomo_fpp_init(GEO3_TO_GEO2(rwy->ends[end].thr), 0,
+		    NULL, B_TRUE);
+		vect2_t thr2thr_v, cross_v, nav_v, c;
+		geo_pos2_t p;
+
+		thr2thr_v = geo2fpp(GEO3_TO_GEO2(rwy->ends[!end].thr), &fpp);
+		nav_v = geo2fpp(GEO3_TO_GEO2(nav->pos), &fpp);
+		cross_v = vect2_norm(thr2thr_v, B_TRUE);
+		c = vect2vect_isect(cross_v, nav_v, thr2thr_v, ZERO_VECT2,
+		    B_FALSE);
+		p = fpp2geo(c, &fpp);
+		nav->loc.corr_pos = GEO_POS3(p.lat, p.lon, nav->pos.elev);
+		nav->loc.brg = dir2hdg(thr2thr_v);
 	}
 	nav->loc.rwy_align_done = B_TRUE;
 }
@@ -837,5 +849,7 @@ navaid_act_freq(navaid_type_t type, uint64_t ref_freq)
 geo_pos3_t
 navaid_get_pos(const navaid_t *nav)
 {
+	if (nav->type == NAVAID_LOC)
+		return (nav->loc.corr_pos);
 	return (nav->pos);
 }

src/libradio/navaiddb.h
@@ -90,6 +90,7 @@ typedef struct {
 		struct {
 			double		brg;		/* degrees */
 			char		rwy_id[8];
+			geo_pos3_t	corr_pos;
 			bool_t		rwy_align_done;
 		} loc;
 		struct {

src/navrad.c
@@ -1785,7 +1785,7 @@ brg2navaid(const navaid_t *nav, double *dist)
 	pos = GEO3_TO_GEO2(navrad.pos);
 	mutex_exit(&navrad.lock);
 
-	fpp = ortho_fpp_init(pos, 0, &wgs84, B_FALSE);
+	fpp = ortho_fpp_init(pos, 0, NULL, B_FALSE);
 	v = geo2fpp(GEO3_TO_GEO2(nav_pos), &fpp);
 
 	if (dist != NULL)

commit 4b75d658e371aad21f1283b4658eb6ede47b37b7

Commit 7324e48 regressed on localizers not paired with runways.

src/navaiddb.c
@@ -746,6 +746,8 @@ loc_align_with_rwy(navaiddb_t *db, navaid_t *nav)
 		p = fpp2geo(c, &fpp);
 		nav->loc.corr_pos = GEO_POS3(p.lat, p.lon, nav->pos.elev);
 		nav->loc.brg = dir2hdg(thr2thr_v);
+	} else {
+		nav->loc.corr_pos = nav->pos;
 	}
 	nav->loc.rwy_align_done = B_TRUE;
 }